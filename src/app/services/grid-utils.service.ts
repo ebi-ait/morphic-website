1230a0f58b717f4b04a7ab312856378f917e7361
Isuru Liyanage <theisuru@gmail.com>
2023-12-11 12:23:19 +0000

    column headers

--- src/app/services/grid-utils.service.ts
+++ src/app/services/grid-utils.service.ts
@@ -6,23 +6,35 @@ import {UrlCellRenderer} from "../url-cell-renderer.component";
   providedIn: 'root'
 })
 export class GridUtilsService {
+
+  public static readonly DEFAULT_COLUMN_DEFINITIONS: ColDef = {
+    flex: 1,
+    minWidth: 100,
+    sortable: true,
+    filter: true,
+    floatingFilter: false,
+    resizable: true,
+    menuTabs: ["filterMenuTab", "generalMenuTab", "columnsMenuTab"],
+    initialHide: true,
+  };
+
   public static readonly COLUMN_DEFINITIONS: ColDef[] = [
     {field: "id"},
-    {field: "dpc", hide: false},
+    {field: "dpc", hide: false, headerName: "DPC"},
     {field: "short_study_label"},
     {field: "upload_status"},
-    {field: "study_title", hide: false},
-    {field: "cell_line", hide: false},
-    {field: "readout_assay", hide: false},
-    {field: "perturbation_type", hide: false},
-    {field: "target_genes", hide: false},
+    {field: "study_title", hide: false, headerName: "Study Title"},
+    {field: "cell_line", hide: false, headerName: "Cell Line"},
+    {field: "readout_assay", hide: false, headerName: "Readout Assay"},
+    {field: "perturbation_type", hide: false, headerName: "Perturbation Type"},
+    {field: "target_genes", hide: false, headerName: "Target Genes"},
     {field: "model_system"},
     {field: "pooled_perturbation"},
     {field: "longitudinal_study"},
     {field: "duo_code_for_data_sharing_restriction"},
     {field: "number_of_datasets"},
-    {field: "expected_release", hide: false, cellDataType: "dateString"},
-    {field: "available_datasets", hide: false, cellRenderer: UrlCellRenderer},
+    {field: "expected_release", hide: false, headerName: "Expected Release", cellDataType: "dateString"},
+    {field: "available_datasets", hide: false, headerName: "Available Datasets", cellRenderer: UrlCellRenderer},
     {field: "publication"},
     {field: "data_upload_contact_name"},
     {field: "data_upload_contact_email_address"},
@@ -35,17 +47,6 @@ export class GridUtilsService {
     {field: "sharing_mechanism_with_DRACC"},
     {field: "comments"},
   ];
-
-  public static readonly DEFAULT_COLUMN_DEFINITIONS: ColDef = {
-    flex: 1,
-    minWidth: 100,
-    sortable: true,
-    filter: true,
-    floatingFilter: false,
-    resizable: true,
-    menuTabs: ["filterMenuTab", "generalMenuTab", "columnsMenuTab"],
-    initialHide: true,
-  };
 
   public static readonly FACET_FIELDS: string[] = ["readout_assay", "perturbation_type", "cell_line"];
 
