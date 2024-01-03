f174ea3f4e3e9a1def30e5f5bd1e76c2837bf476
Isuru Liyanage <theisuru@gmail.com>
2024-01-03 14:46:44 +0000

    fix test cases and pull request suggestions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { GridComponent } from './grid/grid.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {GridComponent} from './grid/grid.component';
 import {AgGridModule} from "ag-grid-angular";
 import {UrlCellRenderer} from "./url-cell-renderer.component";
 import {HttpClientModule} from "@angular/common/http";
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatInputModule} from "@angular/material/input";
 import {MatSelectModule} from "@angular/material/select";
 import {ReactiveFormsModule} from "@angular/forms";
@@ -21,20 +21,21 @@ import {FacetComponent} from "./shared/facet/facet.component";
     GridComponent,
     UrlCellRenderer
   ],
-    imports: [
-        BrowserModule,
-        HttpClientModule,
-        AppRoutingModule,
-        AgGridModule,
-        BrowserAnimationsModule,
-        MatInputModule,
-        MatSelectModule,
-        ReactiveFormsModule,
-        HeaderComponent,
-        FooterComponent,
-        FacetComponent
-    ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    AgGridModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatSelectModule,
+    ReactiveFormsModule,
+    HeaderComponent,
+    FooterComponent,
+    FacetComponent
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
