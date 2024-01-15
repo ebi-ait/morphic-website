ba1f891925ef0e3af595ff8f6d2ebf67a88d492e
Sandeep Selvakumar <sands@ebi.ac.uk>
2024-01-15 17:22:34 +0000

    dc-16: Initial commit with Home Page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {GridComponent} from "./grid/grid.component";
 import {DetailComponent} from "./detail/detail.component";
+import { HomeComponent } from '../home/home.component';
 
 const routes: Routes = [
-  {path: '', component: GridComponent},
+  { path: 'home', component: HomeComponent },
+  { path: '', component: GridComponent },
   { path: 'detail', component: DetailComponent }
+
 ];
 
 @NgModule({
