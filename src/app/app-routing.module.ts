0142a3ade4d18766fdd1aa3da5d9c36223f63976
Sandeep Selvakumar <sands@ebi.ac.uk>
2024-01-16 16:00:12 +0000

    dc-16: Move Data Catalogue to separate route and add links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { GridComponent } from './grid/grid.component';
 
 const routes: Routes = [
-  { path: 'home', component: HomeComponent }
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'data-catalogue', component: GridComponent }
 ];
 
 @NgModule({
