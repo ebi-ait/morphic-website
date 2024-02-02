6d3d0603f69a49a43c92275502f03b95eadafa42
Sandeep Selvakumar <sands@ebi.ac.uk>
2024-02-02 16:53:57 +0000

    dc-64: About Page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {GridComponent} from "./grid/grid.component";
+import { AboutComponent } from './about/about.component';
 import {DetailComponent} from "./detail/detail.component";
 import { HomeComponent } from './home/home.component';
+
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent },
   { path: 'study-tracker', component: GridComponent },
+  { path: 'about', component: AboutComponent },
   { path: 'detail', component: DetailComponent }
 ];
 
