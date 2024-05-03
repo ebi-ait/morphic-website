3dacaec60cc10b195daed346ddc0d02e1157782b
Sandeep Selvakumar <sands@ebi.ac.uk>
2024-05-03 16:42:59 +0100

    Meta robot tag changes to prevent SEO indexing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Meta } from '@angular/platform-browser';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss', './home/home.component.scss']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'study-tracker';
+  constructor(private metaTagService: Meta) {}
+
+  ngOnInit() {
+    this.metaTagService.updateTag({
+      name: 'robots',
+      content: 'noindex, nofollow'
+    });
+  }
 }
