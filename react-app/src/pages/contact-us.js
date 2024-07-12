694f5b4765f9d5e70ba207785b7c1aaac5c8a091
jesse <87054971+jessef10@users.noreply.github.com>
2024-07-11 22:50:18 -0700

    Update Contaus Us page content

--- react-app/src/pages/contact-us.js
+++ react-app/src/pages/contact-us.js
@@ -1,17 +1,16 @@
-import React from "react"
-import Navbar from "../components/Navbar"
-import Footer from "../components/Footer"
-import { StaticImage } from "gatsby-plugin-image"
+import React from "react";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import { StaticImage } from "gatsby-plugin-image";
 
 export default function ContactUs() {
-    
   return (
     <div className="about">
       <div>
         <Navbar />
         <div className="policies-triangle"></div>
       </div>
-      
+
       <div className="about-container">
         <div className="about-info-container">
           <div className="about-info contact-info-bg">
@@ -24,17 +23,26 @@ export default function ContactUs() {
                 <div className="contact-card">
                   <div className="contact-card-body">
                     <h2>Data release enquiries</h2>
-                    <p>For data release enquiries, contact the MorPhiC Data Resource and Administrative Coordinating Center (DRACC)</p>
+                    <p>
+                      For data release enquiries, contact the MorPhiC Data
+                      Resource and Administrative Coordinating Center (DRACC)
+                    </p>
                   </div>
                   <div className="contact-card-footer">
-                    <a>helpdesk@morphic.bio</a></div>
+                    <a href="mailto:helpdesk@morphic.bio">helpdesk@morphic.bio</a>
+                  </div>
                 </div>
+
                 <div className="contact-card">
                   <div className="contact-card-body">
                     <h2>General enquiries</h2>
-                    <p>For any enquiries about the MorPhiC Program Grant, contact</p>
+                    <p>
+                      For any enquiries about the MorPhiC Program Grant, contact
+                    </p>
                   </div>
-                  <div class="contact-card-footer"><a>morphicprogram@mail.nih.gov</a></div>
+                  <div class="contact-card-footer">
+                    <a href="mailto:morphicprogram@mail.nih.gov">morphicprogram@mail.nih.gov</a>
+                  </div>
                 </div>
               </div>
             </section>
@@ -44,5 +52,5 @@ export default function ContactUs() {
         </div>
       </div>
     </div>
-  )
+  );
 }
