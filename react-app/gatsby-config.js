63ef609121ea645538e969d5b55acd7ce35abfe9
Sandeep Selvakumar <sands@ebi.ac.uk>
2024-08-08 15:52:06 +0100

    Add gtag for Dev

--- react-app/gatsby-config.js
+++ react-app/gatsby-config.js
@@ -24,5 +24,29 @@ module.exports = {
       },
     },
     `gatsby-transformer-sharp`, // Needed for dynamic images
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking IDs and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-MK7LFP8XPB", // Google Analytics / GA
+        ],
+        // This object is used for configuration specific to this plugin
+        gtagConfig: {
+          //optimize_id: "OPT_CONTAINER_ID",
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        // This object is used for configuration specific to gtag.js
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          exclude: ["/preview/**", "/do-not-track/me/too/"],
+        },
+      },
+    },
   ],
 }
