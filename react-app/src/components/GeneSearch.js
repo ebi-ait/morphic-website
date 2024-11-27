e20884a8be7929a4e0e6db0e136c4c3b39aeaebb
Sandeep Selvakumar <sands@ebi.ac.uk>
2024-11-27 17:18:00 +0000

    Add morphic-specific genes checkbox and support

--- react-app/src/components/GeneSearch.js
+++ react-app/src/components/GeneSearch.js
@@ -14,6 +14,7 @@ const GeneSearch = () => {
   const [query, setQuery] = useState('');
   const [suggestions, setSuggestions] = useState([]);
   const [isDropdownVisible, setIsDropdownVisible] = useState(false);
+  const [useMorphiCGenesOnly, setUseMorphiCGenesOnly] = useState(false);
 
   // Fetch suggestions function wrapped with debounce
   const fetchSuggestions = useCallback(
@@ -25,7 +26,10 @@ const GeneSearch = () => {
       }
       
       try {
-        const response = await fetch(`https://46ucfedadd.execute-api.us-east-1.amazonaws.com/api/gene-search?query=${searchQuery}`);
+        // Determine the API endpoint based on the checkbox state
+        const apiUrl = useMorphiCGenesOnly
+          ? `https://46ucfedadd.execute-api.us-east-1.amazonaws.com/api/release-1-gene-search?query=${searchQuery}` : `https://46ucfedadd.execute-api.us-east-1.amazonaws.com/api/gene-search?query=${searchQuery}`;
+        const response = await fetch(apiUrl);
         const data = await response.json();
         setSuggestions(data);
         setIsDropdownVisible(true);
@@ -33,7 +37,7 @@ const GeneSearch = () => {
         console.error('Error fetching suggestions:', error);
       }
     }, 300), // 300ms debounce delay
-    []
+    [useMorphiCGenesOnly] // Add the checkbox state as a dependency
   );
 
   // Effect to call fetchSuggestions whenever query changes
@@ -47,16 +51,31 @@ const GeneSearch = () => {
     navigate(`/genes/${hgncId}`);
   };
 
+  // Handle checkbox toggle
+  const handleCheckboxChange = (e) => {
+    setUseMorphiCGenesOnly(e.target.checked);
+  };
+
   return (
     <div className="gene-search">
       <input
         type="text"
         value={query}
         onChange={(e) => setQuery(e.target.value)}
-        placeholder="Search for a gene"
+        placeholder="Search for a gene  e.g. HEY1, PAX6, PPARG"
         onFocus={() => setIsDropdownVisible(true)}
         onBlur={() => setTimeout(() => setIsDropdownVisible(false), 100)}
+        className="icon-search search-box"
       />
+      <div className="checkbox-container">
+        <input
+          type="checkbox"
+          id="morphic-genes-checkbox"
+          checked={useMorphiCGenesOnly}
+          onChange={handleCheckboxChange}
+        />
+        <label htmlFor="morphic-genes-checkbox" className="checkbox-label">MorPhiC genes only</label>
+      </div>
       {isDropdownVisible && suggestions.length > 0 && (
         <ul className="suggestions-list">
           {suggestions.map((suggestion) => (
