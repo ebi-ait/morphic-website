846f7a4a0bd060291a6cc9272844fd7edaceb42a
jessef10 <87054971+jessef10@users.noreply.github.com>
2025-01-21 01:52:05 -0800

    Remove temporary data file

--- react-app/src/pages/data.js
+++ react-app/src/pages/data.js
@@ -4,7 +4,6 @@ import Navbar from "../components/Navbar"
 import Footer from "../components/Footer"
 
 import cover from "../images/external/sangharshlohakare8olkmpo8ugunsplash11571-51ur-800h.png"
-import JSONData from "../../content/studies.json"
 import { Link } from "gatsby"
 import Download from "../components/Data/DownloadDataset"
 import FilterDropdown from "../components/DataTrackerFilters/FilterDropdown"
