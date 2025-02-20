f49ca773e236de271fd9b72660aaa1265b242adc
Sandeep Selvakumar <sands@ebi.ac.uk>
2025-02-20 10:22:15 +0000

    Move news items into a Component and add image for marker-paper

--- react-app/src/pages/news/index.js
+++ react-app/src/pages/news/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Navbar from "../../components/Navbar"
 import Footer from "../../components/Footer"
+import NewsItems from "../../components/NewsItems"
 import { StaticImage } from "gatsby-plugin-image"
 
 import * as styles from "../../styles/news.module.css"
@@ -21,97 +22,7 @@ export default function News() {
             </div>
 
             <section className={`${styles.newsSection} ${styles.newsArticles}`}>
-              <article className={styles.newsArticle}>
-                <div className={styles.newsArticleImgWrap}>
-                    <StaticImage
-                        src="../../images/news-um.png"
-                        alt="Dr. Schürer and members of the MorPhiC DRACC"
-                        placeholder="none"
-                        className={styles.newsArticleImage}
-                    />
-                </div>
-                <div className={styles.newsArticleInfo}>
-                    <time dateTime="2023-03-03">3 March 2023</time>
-                    <h2 className={styles.newsArticleTitle}>The Molecular Phenotypes of Null Alleles in Cells program seeks to identify the purpose behind every gene.</h2>
-                    <p className={styles.newsArticleText}>The Molecular Phenotypes of Null Alleles in Cells program seeks to identify the purpose behind every gene.</p>
-                    <a 
-                        href="https://news.med.miami.edu/dr-stephan-schurer-enlisted-for-important-genome-research-project/" 
-                        target="_blank" rel="noopener noreferrer"
-                        className={styles.newsArticleLink}
-                    >
-                        Read more →
-                    </a>
-                </div>
-              </article>
-
-              <article className={styles.newsArticle}>
-                <div className={styles.newsArticleImgWrap}>
-                    <StaticImage
-                        src="../../images/news-ucsf.jpeg"
-                        alt="Luke Gilbert, UCSF"
-                        placeholder="none"
-                        className={styles.newsArticleImage}
-                    />
-                </div>
-                <div className={styles.newsArticleInfo}>
-                    <time dateTime="2023-09-27">27 September 2022</time>
-                    <h2 className={styles.newsArticleTitle}>NIH initiative to systematically investigate and establish function of every human gene</h2>
-                    <p className={styles.newsArticleText}>NIH initiative to systematically investigate and establish function of every human gene</p>
-                    <a 
-                        href="https://cancer.ucsf.edu/news/2022/09/27/nih-initiative-to-systematically-investigate-and-establish-function-of-every-human" 
-                        target="_blank" rel="noopener noreferrer"
-                        className={styles.newsArticleLink}
-                    >
-                        Read more →
-                    </a>
-                </div>
-              </article>
-
-              <article className={styles.newsArticle}>
-                <div className={styles.newsArticleImgWrap}>
-                    <StaticImage
-                        src="../../images/news-northwestern.png"
-                        alt="Mazhar Adli, PhD"
-                        placeholder="none"
-                        className={styles.newsArticleImage}
-                    />
-                </div>
-                <div className={styles.newsArticleInfo}>
-                    <time></time>
-                    <h2 className={styles.newsArticleTitle}>Identifying How Genes Function to Better Understand Cancer with Mazhar Adli, PhD</h2>
-                    <p className={styles.newsArticleText}>Identifying How Genes Function to Better Understand Cancer with Mazhar Adli, PhD</p>
-                    <a 
-                        href="https://www.feinberg.northwestern.edu/research/podcast/2022/gene-function-to-understand-cancer-adli.html" 
-                        target="_blank" rel="noopener noreferrer"
-                        className={styles.newsArticleLink}
-                    >
-                        Read more →
-                    </a>
-                </div>
-              </article>
-
-              <article className={styles.newsArticle}>
-                <div className={styles.newsArticleImgWrap}>
-                    <StaticImage
-                        src="../../images/news-uw.jpg"
-                        alt="Dr. Ka Yee Yeung and Dr. Ling-Hong Hung posing next to their poster"
-                        placeholder="none"
-                        className={styles.newsArticleImage}
-                    />
-                </div>
-                <div className={styles.newsArticleInfo}>
-                    <time dateTime="2023-05-02">2 May 2023</time>
-                    <h2 className={styles.newsArticleTitle}>Bioinformatics Team Powers International Effort to Map Gene Function</h2>
-                    <p className={styles.newsArticleText}>Identifying How Genes Function to Better Understand Cancer with Mazhar Adli, PhD</p>
-                    <a 
-                        href="https://www.tacoma.uw.edu/news/bioinformatics-team-powers-international-effort-map-gene-function-0" 
-                        target="_blank" rel="noopener noreferrer"
-                        className={styles.newsArticleLink}
-                    >
-                        Read more →
-                    </a>
-                </div>
-              </article>
+              <NewsItems />
 
               <div className={styles.newsArticleBtnWrap}>
                 <button className="button button-orange">Load more</button>
@@ -130,7 +41,7 @@ export function Head() {
   return (
     <>
       <title>MorPhiC program: Molecular Phenotypes of Null Alleles in Cells</title>
-      <link id="icon" rel="icon" href="favicon.svg" />
+      <link id="icon" rel="icon" href="../../favicon.svg" />
     </>
   )
 }
---
+++ react-app/src/utils/constants/NewsArticles.js
@@ -0,0 +1,37 @@
+export const articles = [
+{
+  "date":"13 February 2025",
+  "title": 'A new publication in Nature: "MorPhiC Consortium: towards functional characterization of all human genes"',
+  "subTitle": 'A new publication in Nature: "MorPhiC Consortium: towards functional characterization of all human genes"',
+  "link": "https://www.nature.com/articles/s41586-024-08243-w",
+  "imageSrc": "marker-paper.png"
+},
+{
+  "date":"2 May 2023",
+  "title": "Bioinformatics Team Powers International Effort to Map Gene Function",
+  "subTitle": "Bioinformatics Team Powers International Effort to Map Gene Function",
+  "link": "https://www.tacoma.uw.edu/news/bioinformatics-team-powers-international-effort-map-gene-function-0",
+  "imageSrc": "news-uw.jpg"
+},
+{
+  "date":"3 March 2023",
+  "title": "The Molecular Phenotypes of Null Alleles in Cells program seeks to identify the purpose behind every gene.",
+  "subTitle": "The Molecular Phenotypes of Null Alleles in Cells program seeks to identify the purpose behind every gene.",
+  "link": "https://news.med.miami.edu/dr-stephan-schurer-enlisted-for-important-genome-research-project/",
+  "imageSrc": "news-um.png"
+},
+{
+  "date":"5 Dec 2022",
+  "title": "Identifying How Genes Function to Better Understand Cancer with Mazhar Adli, PhD",
+  "subTitle": "Identifying How Genes Function to Better Understand Cancer with Mazhar Adli, PhD",
+  "link": "https://www.feinberg.northwestern.edu/research/podcast/2022/gene-function-to-understand-cancer-adli.html",
+  "imageSrc": "news-northwestern.png"
+},
+{
+  "date":"27 September 2022",
+  "title": "NIH initiative to systematically investigate and establish function of every human gene",
+  "subTitle": "NIH initiative to systematically investigate and establish function of every human gene",
+  "link": "https://cancer.ucsf.edu/news/2022/09/27/nih-initiative-to-systematically-investigate-and-establish-function-of-every-human",
+  "imageSrc": "news-ucsf.jpeg"
+},
+];
