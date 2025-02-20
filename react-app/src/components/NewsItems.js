f6df2b1f98a1f77533f15e3c095408315bbf1e21
Sandeep Selvakumar <sands@ebi.ac.uk>
2025-02-20 10:22:15 +0000

    Move news items into a Component and add image for marker-paper

---
+++ react-app/src/components/NewsItems.js
@@ -0,0 +1,65 @@
+import React from "react"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
+import { graphql, useStaticQuery } from "gatsby"
+import * as styles from "../styles/news.module.css"
+import { articles } from "../utils/constants/NewsArticles.js"
+
+export default function NewsItems() {
+  const data = useStaticQuery(graphql`
+    query {
+      allFile(filter: { sourceInstanceName: { eq: "images" } }) {
+        nodes {
+          relativePath
+          childImageSharp {
+            gatsbyImageData(width: 600)
+          }
+        }
+      }
+    }
+  `)
+
+  //Map image filenames to Gatsby image objects
+  const imageMap = data.allFile.nodes.reduce((acc, node) => {
+    if (node.childImageSharp) {
+      acc[node.relativePath] = node.childImageSharp.gatsbyImageData;
+    }
+    return acc;
+  }, {})
+
+  return (
+    <>
+      {articles.map((item) => {
+        const imageData = imageMap[item.imageSrc] //Use mapped images
+
+        return (
+          <article key={item.id} className={styles.newsArticle}>
+            <div className={styles.newsArticleImgWrap}>
+              {imageData ? (
+                <GatsbyImage
+                  image={getImage(imageData)}
+                  alt={item.title}
+                  className={styles.newsArticleImage}
+                />
+              ) : (
+                <p>Image not available</p>
+              )}
+            </div>
+            <div className={styles.newsArticleInfo}>
+              <time dateTime={item.date}>{item.date}</time>
+              <h2 className={styles.newsArticleTitle}>{item.title}</h2>
+              <p className={styles.newsArticleText}>{item.subTitle}</p>
+              <a
+                href={item.link}
+                target="_blank"
+                rel="noopener noreferrer"
+                className={styles.newsArticleLink}
+              >
+                Read more →
+              </a>
+            </div>
+          </article>
+        )
+      })}
+    </>
+  )
+}
