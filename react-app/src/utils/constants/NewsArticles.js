61640c3385b46a8e87b2d01f44d852796fd4a740
KociOrges <koci.orges@hotmail.com>
2025-09-15 11:14:47 +0100

    Fix JAX article title/subtitle in NewsArticles.js

--- react-app/src/utils/constants/NewsArticles.js
+++ react-app/src/utils/constants/NewsArticles.js
@@ -1,8 +1,8 @@
 export const articles = [
   {
   "date":"28 August 2025",
-  "title": 'MorPhiC: Cellular modeling at scale" - JAX and their role in the MorPhiC Consortium"',
-  "subTitle": 'MorPhiC: Cellular modeling at scale" - JAX and their role in the MorPhiC Consortium"',
+  "title": '"MorPhiC: Cellular modeling at scale" - JAX speak about their role in the MorPhiC Consortium',
+  "subTitle": '"MorPhiC: Cellular modeling at scale" - JAX speak about their role in the MorPhiC Consortium',
   "link": "https://www.jax.org/news-and-insights/2025/august/morphic-cellular-modeling-at-scale",
   "imageSrc": "cellular-modelling-1.jpg"
 },
