d1444f248d378805a4ea4f8958f7027d41604c81
jessef10 <87054971+jessef10@users.noreply.github.com>
2025-10-31 16:17:26 -0700

    Add publications data

--- react-app/src/utils/constants/PublicationArticles.js
+++ react-app/src/utils/constants/PublicationArticles.js
@@ -1,11 +1,100 @@
 export const publications = [
 {
-    "title": "Title",
-    "journal": "Journal/Conference",
-    "authors": "Authors",
-    "date": "Date",
-    "centers": "Center",
-    "pmid": "ID",
-    "link": ""
+    "title": "Parallel genome-scale CRISPR-Cas9 screens uncouple human pluripotent stem cell identity versus fitness",
+    "journal": "Nature communications",
+    "authors": "Rosen BP, Li QV, Cho HS, Liu D, Yang D, Graff S, Yan J, Luo R, Verma N, Damodaran JR, Kale HT",
+    "date": "2024 Oct 17",
+    "centers": "MSK DPC",
+    "pmid": "39419994",
+    "link": "https://doi.org/10.1038/s41467-024-53284-4"
+},
+{
+    "title": "MorPhiC Consortium: towards functional characterization of all human genes",
+    "authors": "Adli M, Przybyla L, Burdett T, Burridge PW, Cacheiro P, Chang HY, Engreitz JM, Gilbert LA, Greenleaf WJ, Hsu L, Huangfu D",
+    "journal": "Nature",
+    "date": "2025 Feb 13",
+    "centers": "All",
+    "link": "https://doi.org/10.1038/s41586-024-08243-w"
+},
+{
+    "title": "Comparative analysis and directed protein evolution yield an improved degron technology with minimal basal degradation, rapid inducible depletion, and faster recovery of target proteins",
+    "authors": "Adli M, Xing D, Bai T, Neyisci O, Paylakhi S, Duval A, Tekin Y",
+    "journal": "Research Square",
+    "date": "2024 Nov 15",
+    "centers": "NWU DPC",
+    "pmid": "39606491",
+    "link": "https://doi.org/10.21203/rs.3.rs-5348956/v1"
+},
+{
+    "title": "Depletion of lamins B1 and B2 promotes chromatin mobility and induces differential gene expression by a mesoscale-motion-dependent mechanism",
+    "authors": "Pujadas Liwag EM, Wei X, Acosta N, Carter LM, Yang J, Almassalha LM, Jain S, Daneshkhah A, Rao SS, Seker-Polat F, MacQuarrie KL",
+    "journal": "Genome biology",
+    "date": "2024 Mar 22",
+    "centers": "NWU DPC",
+    "pmid": "38519987",
+    "link": "https://doi.org/10.1186/s13059-024-03212-y"
+},
+{
+    "title": "Engineered MED12 mutations drive leiomyoma-like transcriptional and metabolic programs by altering the 3D genome compartmentalization",
+    "authors": "Buyukcelebi K, Chen X, Abdula F, Elkafas H, Duval AJ, Ozturk H, Seker-Polat F, Jin Q, Yin P, Feng Y, Bulun SE",
+    "journal": "Nature communications",
+    "date": "2023 Jul 10",
+    "centers": "NWU DPC",
+    "pmid": "37429859",
+    "link": "https://doi.org/10.1038/s41467-023-39684-y"
+},
+{
+    "title": "Single-nuclei sequencing of uterine serous carcinoma reveals racial differences in immune signaling",
+    "authors": "Foley KG, Adli M, Kim JJ",
+    "journal": "Proceedings of the National Academy of Sciences",
+    "date": "2024 Aug 20",
+    "centers": "NWU DPC",
+    "pmid": "39133838",
+    "link": "https://doi.org/10.1073/pnas.2402998121"
+},
+{
+    "title": "Integrating leiomyoma genetics, epigenomics, and single-cell transcriptomics reveals causal genetic variants, genes, and cell types",
+    "authors": "Buyukcelebi K, Duval AJ, Abdula F, Elkafas H, Seker-Polat F, Adli M",
+    "journal": "Nature communications",
+    "date": "2024 Feb 7",
+    "centers": "NWU DPC",
+    "pmid": "38326302",
+    "link": "https://doi.org/10.1038/s41467-024-45382-0"
+},
+{
+    "title": "Adenomyosis: single-cell transcriptomic analysis reveals a paracrine mesenchymal–epithelial interaction involving the WNT/SFRP pathway",
+    "authors": "Yildiz S, Kinali M, Wei JJ, Milad M, Yin P, Adli M, Bulun SE",
+    "journal": "Fertility and sterility",
+    "date": "2023 May 1",
+    "centers": "NWU DPC",
+    "pmid": "36736810",
+    "link": "https://doi.org/10.1016/j.fertnstert.2023.01.041"
+},
+{
+    "title": "Biology-driven therapy advances in high-grade serous ovarian cancer",
+    "authors": "Wang Y, Duval AJ, Adli M, Matei D",
+    "journal": "The Journal of Clinical Investigation",
+    "date": "2024 Jan 2",
+    "centers": "NWU DPC",
+    "pmid": "38165032",
+    "link": "https://doi.org/10.1172/JCI174013"
+},
+{
+    "title": "Endometriosis and adenomyosis: shared pathophysiology",
+    "authors": "Bulun SE, Yildiz S, Adli M, Chakravarti D, Parker JB, Milad M, Yang L, Chaudhari A, Tsai S, Wei JJ, Yin P",
+    "journal": "Fertility and Sterility",
+    "date": "2023 May 1",
+    "centers": "NWU DPC",
+    "pmid": "36925057",
+    "link": "https://doi.org/10.1016/j.fertnstert.2023.03.006"
+},
+{
+    "title": "HAND1, partially mediated through ape-specific LTR binding, is essential for human extra-embryonic mesenchyme derivation from iPSCs",
+    "authors": "Liu Z, Tan Y, Flynn WF, Sun L, Pratumkaew P, Diniz JA, Oliveira NA, McDonough JA, Skarnes WC, Robson P",
+    "journal": "Cell reports",
+    "date": "2025 Apr 22",
+    "centers": "JAX DPC",
+    "pmid": "40220298",
+    "link": "https://doi.org/10.1016/j.celrep.2025.115568"
 }
 ];
