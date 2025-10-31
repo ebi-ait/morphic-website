c7b35c45695670a4b91725b612412a19283b4cef
jessef10 <87054971+jessef10@users.noreply.github.com>
2025-10-31 15:38:27 -0700

    Add publication section to news page and add left menu

--- react-app/src/pages/news/index.js
+++ react-app/src/pages/news/index.js
@@ -2,6 +2,7 @@ import React from "react"
 import Navbar from "../../components/Navbar"
 import Footer from "../../components/Footer"
 import NewsItems from "../../components/NewsItems"
+import PublicationItems from "../../components/PublicationItems"
 import { StaticImage } from "gatsby-plugin-image"
 import { Seo } from "../../utils/SEO";
 
@@ -15,24 +16,51 @@ export default function News() {
         <div className="policies-triangle"></div>
       </div>
       
-      <div className={`about-container ${styles.newsContainer}`}>
-        <div className="about-info-container">
-          <div className={styles.newsInfo}>
-            <div>
-              <h1 className={styles.newsHeading}>News</h1>
+      <div className="about-container">
+        <div className={styles.container}>
+          <div className={`${styles.grid} ${styles.gap} ${styles.gridColumns3} ${styles.gridColumns1AtMd}`}>
+            
+            {/* Side Menu */}
+            <aside className={`${styles.slideContainer} ${styles.hiddenAtMd}`}>
+              <div className={`${styles.card} ${styles.slidePanel}`}>
+                <h2 className="about-card-title">Insdie this page</h2>
+                <nav>
+                  <ul className={styles.cardList}>
+                    <li><a href="#news">News</a></li>
+                    <li><a href="#publications">Publications</a></li>
+                  </ul>
+                </nav>
+              </div>
+            </aside>
+
+            {/* Main Content */}
+            <div className={styles.marginXAutoAtMd}>
+
+              {/* News Section */}
+              <section id="news" className={`${styles.newsSection} ${styles.newsArticles}`}>
+                <div className="about-section-subtitle-container">
+                  <h1 className={styles.marginNone}>News</h1>
+                </div>
+                <NewsItems />
+
+                {/*<div className={styles.newsArticleBtnWrap}>
+                  <button className="button button-orange">Load more</button>
+                </div>*/}
+              </section>
+
+              {/* Publications Section */}
+              <section id="publications" className={`${styles.newsSection} ${styles.newsArticles} ${styles.marginTop}`}>
+                <div className="about-section-subtitle-container">
+                  <h1 className={styles.marginNone}>Publications</h1>
+                </div>
+
+                <PublicationItems />
+              </section>
             </div>
 
-            <section className={`${styles.newsSection} ${styles.newsArticles}`}>
-              <NewsItems />
-
-              {/*<div className={styles.newsArticleBtnWrap}>
-                <button className="button button-orange">Load more</button>
-              </div>*/}
-            </section>
-
-            <Footer />
           </div>
         </div>
+        <Footer />
       </div>
     </div>
   )
