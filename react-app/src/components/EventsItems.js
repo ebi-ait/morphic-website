de93316151ec4dfc33544a424c974dcff9e47909
jessef10 <87054971+jessef10@users.noreply.github.com>
2025-12-15 14:14:58 -0800

    Add events to news page
    (cherry picked from commit 24726ebd1b2ecc41e5a39bf38e54c5915ff5417b)
    (cherry picked from commit 3452c95c2609bfc83f2c5803b1260f16a70d05ed)

---
+++ react-app/src/components/EventsItems.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import * as styles from "../styles/news.module.css"
+import { events } from "../utils/constants/EventsFeatured.js"
+
+// Utility function, convert date string (YYYY MM DD) to Date object
+const parseDate = (dateStr) => {
+  const [year, month, day] = dateStr.split();
+  return new Date(`${month} ${day}, ${year}`);
+};
+
+// Sort events by date (newest to oldest)
+const sortedEvents = [...events].sort((a, b) => parseDate(b.date) - parseDate(a.date));
+
+
+export default function EventsItems() {
+  return (
+    <>
+      <ul className={`${styles.listNone} ${styles.paddingNone} ${styles.marginNone}`}>
+        {
+          sortedEvents.map((item, index) => {
+            const date = parseDate(item.date);
+            const dateTimeFormat = `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`;
+
+            return (
+              <li key={index}>
+                <article className={`${styles.eventArticle}`}>
+                  <header className={`${styles.grid} ${styles.gridColumns1}`}>
+                    <h2 className={styles.eventTitle}>{item.title}</h2>
+                    <time dateTime={dateTimeFormat} className={`${styles.eventDate} ${styles.orderFirst}`}>{item.date}</time>
+                  </header>
+                  <a className={styles.buttonOrange} href={item.videoLink} target="_blank" rel="noopener noreferrer">Watch Video →</a>
+                </article>
+              </li>
+            )
+          })
+        }
+      </ul>
+    </>
+  )
+}
