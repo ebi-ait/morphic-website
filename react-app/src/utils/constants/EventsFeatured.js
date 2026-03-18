acd2afa6187e4b23151f54b2a7ff1d5a7c6c4a17
jessef10 <87054971+jessef10@users.noreply.github.com>
2026-03-18 14:35:45 -0700

    Edit note paragraph text for event video
    (cherry picked from commit 70a979524f07daae407380f2d2a947d4445e7577)
    (cherry picked from commit 11c984c7d84e5139349135f21cbdd662ada172fd)

--- react-app/src/utils/constants/EventsFeatured.js
+++ react-app/src/utils/constants/EventsFeatured.js
@@ -7,7 +7,7 @@ export const events = [
 {
     "title": "The 106th Meeting of the National Advisory Council for Human Genome Research",
     "date": "2025-09-16",
-    "note": "DRACC presentation starts at 1 hour 51 minutes.",
+    "note": "MorPhiC presentation starts at 1 hour 51 minutes.",
     "videoLink": "https://videocast.nih.gov/watch/d81d7afc-d5db-11f0-9cf9-12c45c580ad9"
 }
 ];
